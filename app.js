07777e93b2b0ffec6f2e353cab7d54f90fc6fe63
Nathan Palmer <nate@thinktheweb.com>
2012-01-19 09:50:59 -0500

    Added mime and a way to serve content that isn't a text file

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var request = require('request');
-var express = require('express');
-var markdown = require('markdown-js')
+var request  = require('request');
+var express  = require('express');
+var markdown = require('markdown-js');
+var sys      = require('util');
+var config   = require('./config');
+var mime     = require('mime');
+
 var app = module.exports = express.createServer();
+var port = process.env.PORT || 3000;
 
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/*', function(req,res) {
-	request('http:/'+req.url, function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			res.render("index.jade", { title: "Home", markdown: markdown, content: body });
-		}
-	});
+app.get('/status', function(req,res) {
+  res.send({ status: "up"});
 });
 
-app.listen(3000);
+app.get('/*', function(req,res) {
+  var url = 'http://'+config.root+req.url;
+  var type = mime.lookup(url);
+
+  sys.log("Requesting " + url);
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      if (type === "text/plain") {  
+        res.render("markdown.jade", { title: "Home", markdown: markdown, 
+content: body });
+      } else {
+         res.writeHead(200, {'Content-Type': type});
+         res.end(body);
+      }
+    } else if (error) {
+      res.render("error.jade", { title: "Error", error: error });
+    }
+  });
+});
+
+app.listen(port);
+
+sys.log("Listening on port " + port);
